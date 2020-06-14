f885c359f40395983c63d0fd1b4a88c77e75e472
Jamie Bray <jamie@jamiebray.xyz>
2020-06-14 22:03:41 +0800

    Improve test development by using array of test constants, rather than it do statements

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -2,31 +2,28 @@ module ParseSpec (parseSpec) where
 
 import Test.Hspec
 import Control.Exception (evaluate)
+import Control.Monad
 import Parse
 
 import Definitions
 
+groupedTests = [
+        ("splitInput", [
+            ("", []),
+            ("1+2", ["1", "+", "2"]),
+            ("123+456", ["123", "+", "456"]),
+            ("1+2-3", ["1", "+", "2", "-", "3"]),
+            ("1-(1+1)", ["1", "-(", "1", "+", "1", ")"]),
+            ("2SIN", ["2", "SIN"]),
+            ("1+2SIN/1", ["1", "+", "2", "SIN/", "1"])
+        ])
+    ]
+
 parseSpec :: IO ()
 parseSpec = hspec $ do
-  describe "splitInput" $ do
+    forM_ groupedTests $ \(label, tests) -> do
+        describe label $ do
+            forM_ tests $ \(input, output) -> do
+                it input $ do
+                    splitInput input `shouldBe` output
 
-    it "'' => []" $ do
-      splitInput "" `shouldBe` []
-
-    it "'123' => []" $ do
-      splitInput "123" `shouldBe` ["123"]
-
-    it "'1+2' => []" $ do
-      splitInput "1+2" `shouldBe` ["1", "+", "2"]
-
-    it "'123+456' => []" $ do
-      splitInput "123+456" `shouldBe` ["123", "+", "456"]
-
-    it "'1+2-3' => []" $ do
-      splitInput "1+2-3" `shouldBe` ["1", "+", "2", "-", "3"]
-
-    it "'2`sin`' => []" $ do
-      splitInput "2SIN" `shouldBe` ["2", "SIN"]
-
-    it "'1-(1+1)' => []" $ do
-      splitInput "1-(1+1)" `shouldBe` ["1", "-", "(", "1", "+", "1", ")"]
